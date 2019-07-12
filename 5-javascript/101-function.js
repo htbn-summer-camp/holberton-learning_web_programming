03a8096a54ba899e99f8df25287134c56d5ff8ab
htbn-summer-camp <52672933+htbn-summer-camp@users.noreply.github.com>
2019-07-12 09:39:33 -0700

    Create 101-function.js

---
+++ 5-javascript/101-function.js
@@ -0,0 +1,10 @@
+let b = myFunction1("Hello World")
+function myFunction1(b) {
+  return b;   
+}
+console.log(b)
+let a = myFunction2(prompt())
+function myFunction2(a) {
+  return a * 2;   
+}
+console.log(a)
